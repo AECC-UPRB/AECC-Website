9938f54dfb62543788da2c6b6cc8614f08de57de
juanjalvarez <jalvarez5241997@gmail.com>
2016-02-08 23:43:13 -0400

    Greatly improved about page

--- test/db_interactive.rb
+++ test/db_interactive.rb
@@ -7,29 +7,32 @@ begin
 	until input == "quit" do
 		puts "Command:"
 		input = gets.chomp
-		if input == "create" then
-			load 'db_create.rb'
-		elsif input == "insert" then
-			load 'db_insert.rb'
-		elsif input == "destroy" then
-			load 'db_destroy.rb'
-		elsif input == "drop" then
-			load 'db_drop.rb'
-		elsif input == "view" then
-			load 'db_view.rb'
-		elsif input == "refresh" then
-			load 'db_drop.rb'
-			load 'db_create.rb'
-			load 'db_insert.rb'
-		elsif input == "exec" then
-			puts "Query:"
-			code = gets.chomp
-			result = con.exec(code)
-			result.each do|row|
-				puts row
+		unless input == "quit" then
+			if input == "create" then
+				load 'db_create.rb'
+			elsif input == "insert" then
+				load 'db_insert.rb'
+			elsif input == "destroy" then
+				load 'db_destroy.rb'
+			elsif input == "drop" then
+				load 'db_drop.rb'
+			elsif input == "view" then
+				load 'db_view.rb'
+			elsif input == "refresh" then
+				load 'db_drop.rb'
+				load 'db_create.rb'
+				load 'db_insert.rb'
+			elsif input == "exec" then
+				puts "Query:"
+				code = gets.chomp
+				result = con.exec(code)
+				puts result
+				result.each do|row|
+					puts row
+				end
+			else
+				puts "'#{input}' is not a known command"
 			end
-		else
-			puts "'#{input}' is not a known command"
 		end
 	end
 	puts "Exiting interactive"
